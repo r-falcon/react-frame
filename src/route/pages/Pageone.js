83887bf07e556ae0b55cecee3f33a6b1be246a6e
r-falcon <rfalcon@163.com>
2021-12-13 11:11:28 +0800

    -生命周期+定时器在组件中的挂载和卸载

--- src/route/pages/Pageone.js
+++ src/route/pages/Pageone.js
@@ -1,5 +1,40 @@
 import React from 'react'
 
+/**
+ * 定时器的挂载和卸载
+ */
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: new Date(),
+    }
+  }
+
+  componentDidMount() {
+    //挂载
+    console.log('挂载')
+    this.timerID = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    //卸载
+    console.log('卸载')
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    })
+  }
+
+  render() {
+    return <div>right now is : {this.state.date.toLocaleTimeString()}</div>
+  }
+}
+
 class Pageone extends React.Component {
   constructor(props) {
     super(props)
@@ -7,7 +42,12 @@ class Pageone extends React.Component {
   }
 
   render() {
-    return <div>page one</div>
+    return (
+      <div>
+        page one
+        <Clock />
+      </div>
+    )
   }
 }
 
