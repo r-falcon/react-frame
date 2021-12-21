c851f5d879659b0d19f141abcd96a802adf0696a
r-falcon <rfalcon@163.com>
2021-12-21 14:24:35 +0800

    -高阶函数样例

---
+++ src/hooks/HOC.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+class Example extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          current value is,x:{this.props.x},y:{this.props.y}
+        </p>
+        <button onClick={this.props.handleClick}>HOC click it</button>
+      </div>
+    )
+  }
+}
+
+const HOC = (Component) => {
+  return class extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        x: 0,
+        y: 0,
+      }
+    }
+
+    handleClick = () => {
+      this.setState({
+        x: this.state.x + 1,
+        y: this.state.y + 1,
+      })
+    }
+
+    render() {
+      return (
+        <Component
+          {...this.props}
+          {...this.state}
+          handleClick={this.handleClick}
+        />
+      )
+    }
+  }
+}
+
+export default HOC(Example)
--- src/hooks/HookPanel.js
+++ src/hooks/HookPanel.js
@@ -3,12 +3,13 @@ import State from './state'
 import UseState from './UseState'
 import RenderProps from './RenderProps'
 import ChildrenProps from './ChildrenProps'
+import HOC from './HOC'
 
 /**
  * Hooks
  * 无状态组件（Function），无生命周期、无this、无state
  * 状态组件（Class）,有生命周期，有this，有state
- * 想要复用一个状态组件：渲染属性（render props）、高阶组件（higher-order-components）
+ * 想要复用一个状态组件：渲染属性（render props）[this.props.children。注：props可以传递任何对象，包括组件和函数]、高阶组件（higher-order-components）[参数为组件，返回值为新组件的函数]
  */
 
 class HookPanel extends React.Component {
@@ -19,6 +20,7 @@ class HookPanel extends React.Component {
         <UseState />
         <RenderProps />
         <ChildrenProps />
+        <HOC />
       </Fragment>
     )
   }
