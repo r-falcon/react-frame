fe2701bf1c02ef610edfbb4b888de18810167428
r-falcon <rfalcon@163.com>
2021-12-21 17:49:12 +0800

    -

--- src/hooks/modules/UseEffect.js
+++ src/hooks/modules/UseEffect.js
@@ -3,6 +3,14 @@ import React, { useEffect, useState } from 'react'
 function UseEffect() {
   const [count, setCount] = useState(0)
 
+  /**
+   * useEffect 第二个参数传递变量
+   * 1.第二个参数可以不传，不会报错，浏览器会无限循环执行逻辑处理函数
+   * 2.第二个参数为空数组，逻辑处理函数只会在组件挂载时执行一次[componentDidMount + componentWillUnmount]
+   * 3.第二个参数不为空，在首次挂载时执行和变量更新时执行[componentDidMount + componentDidUpdate]
+   * 4.第一个参数可以返回一个回调，这个回调函数将会在组件被摧毁之前和再一次触发更新时，将之前的副作用清除掉[componentWillUnmount]
+   */
+
   // useEffect(() => {
   //   document.title = `Click ${count} times`
   // })
