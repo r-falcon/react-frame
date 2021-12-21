9271097caa03036aa3abbc1d70dc6a1644d56e89
r-falcon <rfalcon@163.com>
2021-12-21 11:36:07 +0800

    -this.props.children

---
+++ src/hooks/ChildrenProps.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+class ChildrenProps extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      x: 0,
+      y: 0,
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      x: this.state.x + 1,
+      y: this.state.y + 1,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.children(this.state)}
+        <button onClick={this.handleClick}>click it</button>
+      </div>
+    )
+  }
+}
+
+class Props extends React.Component {
+  render() {
+    return (
+      <div>
+        current value is,x:{this.props.params.x},y:{this.props.params.y}
+      </div>
+    )
+  }
+}
+
+class Children extends React.Component {
+  render() {
+    return (
+      <ChildrenProps>{(params) => <Props params={params} />}</ChildrenProps>
+    )
+  }
+}
+
+export default Children
