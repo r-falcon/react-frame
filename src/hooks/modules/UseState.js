a294ca8f15854c5a818a1a2cd4f0b2ee74403257
r-falcon <rfalcon@163.com>
2021-12-21 16:50:38 +0800

    -useState,函数式更新

--- src/hooks/modules/UseState.js
+++ src/hooks/modules/UseState.js
@@ -3,6 +3,12 @@ import { useState } from 'react'
 function UseState() {
   const [count, setCount] = useState(0)
 
+  const lazyAdd = () => {
+    setTimeout(() => {
+      setCount((count) => Number(count) + 2)
+    }, 2000)
+  }
+
   return (
     <div>
       <p>hook count is:{count}</p>
@@ -13,6 +19,7 @@ function UseState() {
         onChange={(e) => setCount(e.target.value)}
       />
       <button onClick={() => setCount(count + 1)}>count++</button>
+      <button onClick={lazyAdd}>function update count++</button>
     </div>
   )
 }
