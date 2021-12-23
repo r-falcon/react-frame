0518079634f8b7d249e8245c9d68d0fcaa9f641c
r-falcon <rfalcon@163.com>
2021-12-23 10:42:32 +0800

    -封装自定义组件useInput

--- src/hooks/modules/CustomHooks.js
+++ src/hooks/modules/CustomHooks.js
@@ -1,11 +1,13 @@
 import { Fragment, useState } from 'react'
 import useDocumentTitle from './custom/useDocumentTitle'
 import useCounter from './custom/useCounter'
+import useInput from './custom/useInput'
 
 function CustomHooks() {
   return (
     <Fragment>
       <Demo />
+      <FormDemo />
     </Fragment>
   )
 }
@@ -38,6 +40,48 @@ function Demo() {
       <button onClick={increment}>count++</button>
       <button onClick={decrement}>count--</button>
       <button onClick={reset}>reset</button>
+    </div>
+  )
+}
+
+function FormDemo() {
+  // const [firstName, setFirstName] = useState('')
+  // const [lastName, setLastName] = useState('')
+
+  // 封装useInput
+  const [firstName, bindFirstName, resetFirstName] = useInput('')
+  const [lastName, bindLastName, resetLastName] = useInput('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(`Hello,${firstName}-${lastName}`)
+    resetFirstName()
+    resetLastName()
+  }
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="">First Name</label>
+          {/* <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          /> */}
+          <input type="text" {...bindFirstName} />
+        </div>
+        <div>
+          <label htmlFor="">Last Name</label>
+          {/* <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          /> */}
+          <input type="text" {...bindLastName} />
+        </div>
+        <button>submit</button>
+      </form>
     </div>
   )
 }
